9d84fc2199e6da320ca0756205291f18fa698d37
imcousin <cusson12@gmail.com>
2021-12-03 08:14:44 -0800

    add login route

--- server/index.js
+++ server/index.js
@@ -32,3 +32,44 @@ const PORT = process.env.PORT || 8080;
 mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
+
+
+
+
+// Routes
+app.post('/users/login', async (req, res) => {
+  console.log('/users/login');
+  console.log(req.body);
+
+  const user = await User.findOne({
+    email: req.body.email
+  })
+
+  if (!user) {
+    console.log('user not in db');
+    return res.status(400).json({ status: 'error', error: 'Invalid Login' })
+  }
+
+  // Check if user password is correct with db
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  )
+
+  if (isPasswordValid) {
+    console.log('pw valid');
+    const token = jwt.sign({
+      email: user.email,
+      admin: user.admin
+    }, 'secret123')
+
+    return res.status(200).json({ user: token })
+  }
+  else {
+    return res.status(400).json({ status: 'error', user: false })
+  }
+})
+
+app.get('assignments', () => {
+  
+})
